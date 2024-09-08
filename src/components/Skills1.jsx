99cb79b3b2d9a53735dd10d67f082a1efeb6d306
kousigaraj77 <kousigaraj77@gmail.com>
2024-09-08 04:16:33 +0000

    made some changes

--- src/components/Skills1.jsx
+++ src/components/Skills1.jsx
@@ -12,18 +12,18 @@ const Skills1 = () => {
 
   const skills = {
     frontEnd: [
-      { percentage: 90, image: "/assets/html-logo.png", name: "HTML", alt: "html-logo" },
-      { percentage: 80, image: "/css-logo.svg", name: "CSS", alt: "css-logo" },
-      { percentage: 50, image: "/javaScript-logo.svg", name: "JavaScript", alt: "javaScript-logo" },
-      { percentage: 50, image: "/react.svg", name: "React JS", alt: "react-logo" },
-      { percentage: 50, image: "/bootstrap-logo.svg", name: "Bootstrap", alt: "bootstrap-logo" }
+      { percentage: 90, image: "/assets/html-logo.svg", name: "HTML", alt: "html-logo" },
+      { percentage: 80, image: "/assets/css-logo.svg", name: "CSS", alt: "css-logo" },
+      { percentage: 50, image: "/assets/javaScript-logo.svg", name: "JavaScript", alt: "javaScript-logo" },
+      { percentage: 50, image: "/assets/react.svg", name: "React JS", alt: "react-logo" },
+      { percentage: 50, image: "/assets/bootstrap-logo.svg", name: "Bootstrap", alt: "bootstrap-logo" }
     ],
     backEnd: [
-      { percentage: 0, image: "/nodejs-logo.svg", name: "Node JS", alt: "nodejs-logo" },
-      { percentage: 0, image: "/expressjs-logo.svg", name: "Express JS", alt: "expressjs-logo" },
-      { percentage: 30, image: "/python-logo.svg", name: "Python", alt: "python-logo" },
-      { percentage: 0, image: "/mongodb-logo.svg", name: "MongoDB", alt: "mongodb-logo" },
-      { percentage: 60, image: "/mysql-logo.svg", name: "MySQL", alt: "mysql-logo" }
+      { percentage: 0, image: "/assets/nodejs-logo.svg", name: "Node JS", alt: "nodejs-logo" },
+      { percentage: 0, image: "/assets/expressjs-logo.svg", name: "Express JS", alt: "expressjs-logo" },
+      { percentage: 30, image: "/assets/python-logo.svg", name: "Python", alt: "python-logo" },
+      { percentage: 0, image: "/assets/mongodb-logo.svg", name: "MongoDB", alt: "mongodb-logo" },
+      { percentage: 60, image: "/assets/mysql-logo.svg", name: "MySQL", alt: "mysql-logo" }
     ]
   };
 
